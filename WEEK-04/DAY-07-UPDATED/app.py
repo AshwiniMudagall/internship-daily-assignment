646596b9a5a4355f646b7905906f40364f4062b5
AshwiniMudagall <mudagallashwini213@gmail.com>
2026-03-06 21:39:44 +0530

    Update app.py

--- WEEK-04/DAY-07-UPDATED/app.py
+++ WEEK-04/DAY-07-UPDATED/app.py
@@ -95,8 +95,10 @@ def items():
     data = cursor.fetchall()
 
     return render_template("items.html", items=data)
-if __name__=="__main__":
-    app.run(debug=True)
+# if __name__=="__main__":
+#     app.run(debug=True)
+if __name__=="__main__": 
+    app.run(host="0.0.0.0",port=10000)
 
 
 
